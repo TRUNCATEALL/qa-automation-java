77d57a0cb4bf311608821668eb869d4c94bc739b
an.solovyev <an.solovyev@tinkoff.ru>
2021-08-30 17:46:18 +0300

    fix4 by review (move methods from LoanCalcService)

--- app/src/main/java/com/tinkoff/edu/app/service/LoanCalcService.java
+++ app/src/main/java/com/tinkoff/edu/app/service/LoanCalcService.java
@@ -1,50 +1,8 @@
 package com.tinkoff.edu.app.service;
 
-import com.tinkoff.edu.app.dictionary.ClientType;
-import com.tinkoff.edu.app.dictionary.LoanResponseStatus;
 import com.tinkoff.edu.app.model.LoanRequest;
 import com.tinkoff.edu.app.model.LoanResponse;
 
-import java.math.BigDecimal;
-
-import static com.tinkoff.edu.app.dictionary.LoanResponseStatus.APPROVED;
-import static com.tinkoff.edu.app.dictionary.LoanResponseStatus.DECLINED;
-
 public interface LoanCalcService {
     LoanResponse createRequest(LoanRequest loanRequest);
-
-    default LoanResponseStatus getResponseStatus(ClientType clientType, BigDecimal cornerAmount, BigDecimal amount, int months) {
-        switch (clientType) {
-            case PERSON:
-                return getRespStatusForPerson(cornerAmount, amount, months);
-            case OOO:
-                return getRespStatusForOoo(cornerAmount, amount, months);
-            case IP:
-                return getRespStatusForIp();
-            default:
-                throw new NullPointerException("Неизвестный тип клиента");
-        }
-    }
-
-    default LoanResponseStatus getRespStatusForPerson(BigDecimal cornerAmount, BigDecimal amount, int months) {
-
-        if (amount.compareTo(cornerAmount) <= 0 && months <= 12) {
-            return APPROVED;
-        } else {
-            return DECLINED;
-        }
-    }
-
-    default LoanResponseStatus getRespStatusForOoo(BigDecimal cornerAmount, BigDecimal amount, int months) {
-
-        if (amount.compareTo(cornerAmount) > 0 && months <= 12) {
-            return APPROVED;
-        } else {
-            return DECLINED;
-        }
-    }
-
-    default LoanResponseStatus getRespStatusForIp() {
-        return DECLINED;
-    }
 }
--- app/src/main/java/com/tinkoff/edu/app/service/SimpleLoanCalcService.java
+++ app/src/main/java/com/tinkoff/edu/app/service/SimpleLoanCalcService.java
@@ -1,11 +1,15 @@
 package com.tinkoff.edu.app.service;
 
+import com.tinkoff.edu.app.dictionary.ClientType;
 import com.tinkoff.edu.app.dictionary.LoanResponseStatus;
 import com.tinkoff.edu.app.model.LoanRequest;
 import com.tinkoff.edu.app.model.LoanResponse;
 import com.tinkoff.edu.app.repository.LoanCalcRepository;
 
 import java.math.BigDecimal;
+
+import static com.tinkoff.edu.app.dictionary.LoanResponseStatus.APPROVED;
+import static com.tinkoff.edu.app.dictionary.LoanResponseStatus.DECLINED;
 
 /**
  * Loan calculation
@@ -34,5 +38,40 @@ public class SimpleLoanCalcService implements LoanCalcService {
         LoanResponseStatus loanResponseStatus = getResponseStatus(loanRequest.getClientType(), cornerAmount, requestAmount, requestMonths);
 
         return loanCalcRepository.save(loanRequest, loanResponseStatus);
+    }
+
+    private LoanResponseStatus getResponseStatus(ClientType clientType, BigDecimal cornerAmount, BigDecimal amount, int months) {
+        switch (clientType) {
+            case PERSON:
+                return getRespStatusForPerson(cornerAmount, amount, months);
+            case OOO:
+                return getRespStatusForOoo(cornerAmount, amount, months);
+            case IP:
+                return getRespStatusForIp();
+            default:
+                throw new NullPointerException("Неизвестный тип клиента");
+        }
+    }
+
+    private LoanResponseStatus getRespStatusForPerson(BigDecimal cornerAmount, BigDecimal amount, int months) {
+
+        if (amount.compareTo(cornerAmount) <= 0 && months <= 12) {
+            return APPROVED;
+        } else {
+            return DECLINED;
+        }
+    }
+
+    private LoanResponseStatus getRespStatusForOoo(BigDecimal cornerAmount, BigDecimal amount, int months) {
+
+        if (amount.compareTo(cornerAmount) > 0 && months <= 12) {
+            return APPROVED;
+        } else {
+            return DECLINED;
+        }
+    }
+
+    private LoanResponseStatus getRespStatusForIp() {
+        return DECLINED;
     }
 }
